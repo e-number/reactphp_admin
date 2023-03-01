00121aa0895224d3018fb41eb87550253c400519
eggnom <h75408706@gmail.com>
2023-03-01 19:28:23 +0300

    react

--- app/src/main.js
+++ app/src/main.js
@@ -1,23 +1,7 @@
-import $ from 'jquery'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Editor from './components/editor';
 
-function getPageList() {
-    $("h1").remove();
-    $.get("./api", data => {
-        data.forEach(file => {
-            $("body").append(`<h1>${file}</h1>`)
-        })
-    }, "JSON");
-}
-
-getPageList();
-
-$("button").click(() => {
-    $.post("./api/createNewPage.php", {
-        "name": $("input").val()
-    }, () => {
-        getPageList();
-    })
-        .fail(() => {
-            alert("Страница уже существует!");
-        })
-});
+// ReactDOM.createRoot(<Editor />, document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+root.render(<Editor />);
